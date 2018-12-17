717d4a84f9c1ca568bce88991ff4604282a4cda0
Moon <moontasirul01@gmail.com>
2018-12-18 01:59:04 +0600

    ui update

--- app/src/main/java/com/project/universityproject/theifmonitoring/MainActivity.java
+++ app/src/main/java/com/project/universityproject/theifmonitoring/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mainnew);
         RunTimePermissionHandler.requestForAccessDeviceLocationAndPhoneState(this);
         initialView();
         setOnClick();
